94a43789c8081f11cc6d2f8f653a702072953879
gutohertzog <guto.hertzog@gmail.com>
2025-06-11 21:21:16 -0300

    atualização da tabela

--- uc3-planejar-administrar-banco-dados/queries/tabela.sql
+++ uc3-planejar-administrar-banco-dados/queries/tabela.sql
@@ -2,11 +2,12 @@ drop table jogos;
 
 create table jogos
 (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nome varchar(200) NOT NULL,
-    genero varchar(100) NOT NULL,
-    plataforma varchar(100) NOT NULL,
-    qtd_jogadores SMALLINT UNSIGNED NOT NULL,
-    metascore TINYINT NULL,
-    ano_lanc SMALLINT UNSIGNED NOT NULL
+    id INT AUTO_INCREMENT PRIMARY KEY, -- identificador do jogo
+    nome varchar(200) NOT NULL, -- nomo de jogo
+    genero varchar(100) NOT NULL, -- gênero do jogo (tiro, rpg, etc)
+    console varchar(100) NOT NULL, -- plataforma de lançamento (nintendo 64, playstation 3, etc)
+    publicadora varchar(100) NOT NULL, -- empresa que publica o jogo (sony, nintendo, etc)
+    qtd_jogadores SMALLINT UNSIGNED NOT NULL, -- quantidade de jogadores local ou online
+    metascore TINYINT NULL, -- nota do jogo na plataforma no metascore
+    ano_lanc SMALLINT UNSIGNED NOT NULL -- ano de lançamento do jogo
 );
